7844d708dd823578c6b38872cb304e4b9310e86b
“sahilpandith” <sahil.romit@gmail.com>
2023-12-24 22:40:59 +0530

    solution

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+import { useState ,useRef,useEffect} from 'react'
+import './App.css';
+import fetchData from './mockApi';
+
+function App() {
+  const [score, setScore] = useState(0);
+  const bestScore = useRef(0);
+  const [data,setData] = useState([]);
+  const [hitUrls,setHitUrls] = useState([]);
+  
+  function clearStates(){
+    setScore(0);
+    setHitUrls([]);
+  }
+  function setBestScore(score){
+    if(score>bestScore.current){
+      bestScore.current=score;
+    }
+  }
+  function imgHandler(url){
+    if(hitUrls.includes(url)){
+      alert('game over');
+      setBestScore(score);
+      clearStates();
+    }else if(hitUrls.length===12){
+      alert('won');
+      setBestScore(score);
+      clearStates();
+    }else{
+      setHitUrls([...hitUrls,url]); 
+      setScore(score+1);
+    }
+  }
+
+  function shuffle (array){ 
+    return array.map((a) => ({ sort: Math.random(), value: a }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value); 
+  }
+
+  useEffect(() => {
+    setData([...shuffle(data)]);
+  },[score])
+
+  useEffect(() => {
+        async function getData(){
+          const response = await fetchData();
+          setData(response);
+        }
+        getData();
+  },[])
+
+  return (
+    <div className='app'>
+      <header>
+        <div className='game-name'>Memory Game</div>
+        <div className="scorecard">
+            <div className="current-score"><span>Score : </span>{score}</div>
+            <div className="best-score"><span>Best Score : </span>{bestScore.current}</div>
+        </div>
+      </header>
+      <main>
+        {data.map((url) => <div className='image' key={url}>
+                            <img src={url} onClick={() => imgHandler(url)}></img>
+                          </div>
+          )}
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/mockApi.js
@@ -0,0 +1,19 @@
+async function fetchData(){
+    const data = [
+        "https://heldersrvio.github.io/memory-card-game/static/media/anne.d9d14c90.jpg",
+        "https://heldersrvio.github.io/memory-card-game/static/media/croaker.824ac0d7.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/grime.6e597003.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/hoppop.5badcd86.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/loggle.c5790b4e.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/maddie.794635a6.jpg",
+        "https://heldersrvio.github.io/memory-card-game/static/media/polly.7702daf7.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/sasha.5e94ec83.jpg",
+        "https://heldersrvio.github.io/memory-card-game/static/media/sprig.57a1fbf3.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/sylvia.fc5ee06d.png",
+        "https://heldersrvio.github.io/memory-card-game/static/media/toadstool.2b499bfe.jpg",
+        "https://heldersrvio.github.io/memory-card-game/static/media/wally.8036609c.png"
+    ]
+    return Promise.resolve(data)
+}
+
+export default fetchData;
